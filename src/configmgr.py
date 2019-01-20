4f58ba73b0b8dd927e7271188e8c9397c3983db2
Misha Turnbull <mishaturnbull@gmail.com>
2019-01-19 20:06:50 -0600

    Add service config reader integration with Service class
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- src/configmgr.py
+++ src/configmgr.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import json
+from servicemgr import Service
 
 
 class ConfigReader (object):
 
-    def __init__(self, filename):
+    def __init__(self, filename, coordinator):
         """Read config data from filename."""
         self._filename = filename
         self._data = None
+        self._services = []
+        self.coordinator = coordinator
 
     def _read_file(self):
         """Read & parse data."""
         with open(self._filename, 'r') as cfg:
             contents = cfg.read()
         self._data = json.loads(contents)
+        for service in self._data['services']:
+            self._services.append(
+                Service(service['name'], service['computer_name'],
+                        service['start'], service['stop'], self.coordinator)
+            )
 
     @property
     def services_list(self):
         """Get a list of the services provided in the config."""
-        return self._data['services']
+        return self._services
