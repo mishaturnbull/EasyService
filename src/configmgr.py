37a178cbfacc6ff04a52265ef5b5cfec874df8fb
Misha Turnbull <mishaturnbull@gmail.com>
2019-01-19 21:13:05 -0600

    Add category type to services
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- src/configmgr.py
+++ src/configmgr.py
@@ -12,19 +12,36 @@ class ConfigReader (object):
         self._data = None
         self._services = []
         self.coordinator = coordinator
+        self._categories = {}
 
     def _read_file(self):
         """Read & parse data."""
         with open(self._filename, 'r') as cfg:
             contents = cfg.read()
         self._data = json.loads(contents)
+        categories = self._data['categories']
+
+        for category in categories:
+            self._categories.update({category: []})
+
         for service in self._data['services']:
-            self._services.append(
-                Service(service['name'], service['computer_name'],
-                        service['start'], service['stop'], self.coordinator)
-            )
+            svo = Service(service['name'], service['computer_name'],
+                          service['start'], service['stop'],
+                          service['category'], self.coordinator)
+            self._services.append(svo)
+            self._categories[service['category']].append(svo)
+
+
+    def read(self):
+        """Public interface for reading & parsing data.  Does not return."""
+        self._read_file()
 
     @property
-    def services_list(self):
+    def services(self):
         """Get a list of the services provided in the config."""
         return self._services
+
+    @property
+    def categories(self):
+        """Get a dictionary of services by categorization."""
+        return self._categories
